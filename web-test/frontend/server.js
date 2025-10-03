121a9986589fe47b0f95532c2a57b856d73206b1
Panavat0113 <141387589+Panavat0113@users.noreply.github.com>
2025-10-03 19:26:55 +0700

    Update server.js

--- web-test/frontend/server.js
+++ web-test/frontend/server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname)));
 
+app.get("/", (req, res) => {
+    res.send('Home');
+});
+
 app.listen(PORT, () => {
   console.log(`Frontend running on http://localhost:${PORT}`);
 });
