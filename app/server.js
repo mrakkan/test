b6a14f324859bdb11c03c41a800020eb101d96cc
mrakkan <mansuperyoung4399@gmail.com>
2025-10-04 14:58:06 +0700

    server.js config

--- app/server.js
+++ app/server.js
@@ -6,11 +6,14 @@ const app = express();
 // Serve static files
 app.use(express.static(path.join(__dirname)));
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Frontend running`);
-});
-
 app.get("/health", (req, res) => {
   res.send("OK");
 });
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+
+
+
