df9e15a29859ea5dee95cc43d3fa172dd97071c3
mrakkan <mansuperyoung4399@gmail.com>
2025-10-04 14:53:53 +0700

    healthcheck add

--- app/server.js
+++ app/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+
 
 // Serve static files
 app.use(express.static(path.join(__dirname)));
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Frontend running on http://localhost:${PORT}`);
+  console.log(`Frontend running`);
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
